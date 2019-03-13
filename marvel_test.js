66e972b3e17b28dbabd2a5e688e1ae2a481fbe4e
jialululu <jialutan@mit.edu>
2019-03-12 20:57:38 -0400

    modified second json

--- marvel_test.js
+++ marvel_test.js
@@ -4,7 +4,7 @@ var groupSep =  10; // the distance between different nodes
 // controls the node radius and the distance between links
 // var noderadius = d3.scaleSqrt().range([2,4]); // set five levels for result node radius, range is the result value
 var noderadius = 2;
-// var linkwidth = d3.scaleLinear().range([1.5, 2*noderadius.range()[0]]);  // the thickest line should be no more than the smallest node diameter
+var linkwidth = d3.scaleLinear().range([1.5, 4]);  // the thickest line should be no more than the smallest node diameter
 
 // the margin of the svg
 // var margin = {
@@ -17,8 +17,8 @@ var noderadius = 2;
 var margin = {
     top: 3,
     bottom:3,
-    left:3,
-    right:3
+    left:5,
+    right:5
 };
 
 var width = 1200 - margin.left - margin.right;
@@ -34,6 +34,8 @@ var svg = d3.select('#network-graph')
     .append('g');
 
 console.log(svg);
+
+var path =
 
 // main visualization
 
@@ -56,7 +58,7 @@ data = d3.json('Data/data_co.json', function(error, graph){
 
     // set node radius and link width
     // noderadius.domain(d3.extent(graph.nodes, function(d){return d.APPEARANCES;}));
-    // linkwidth.domain(d3.extent(graph.links, function(d){return d.count;}));
+    linkwidth.domain(d3.extent(graph.links, function(d){return d.count;}));
 
     // compute node
     var node = svg.append('g')
@@ -68,18 +70,63 @@ data = d3.json('Data/data_co.json', function(error, graph){
         .append('circle')
         // .attr('cx', function(d){return d.x; })
         // .attr('cy', function(d){return height-noderadius.range()[0]; })
-        .attr('cx', function(d,i){return i*width/155;})
+        .attr('fill', 'rgba(100,107,110,0.8)')
+        .attr('stroke', '#191c33')
+        .attr('stroke-width', '1px')
+        .attr('cx', function(d,i){return i*width/(this.length);})
         .attr('cy', function(d,i){return height;})
         .attr('r', function(d){return noderadius; }) //change circle radius based on the magnitute of links
-    //     .on('mouseover', function(d){
-    //         node.style('fill', null);
-    //         d3.select(this).style('fill', 'orange');
-    //         var nodesToHighlight = graph.map(function(e){return e.})
-    //
-    //     })
+        .on('mouseover', function(d){
+            node.style('fill', null);
+
+            d3.select(this).style('fill', '#d66742'); // the node with your mouse on
+
+            var nodesToHighlight = graph.links.map(function(e){return e.source === d ?
+                e.target : e.target === d ? e.source : 0})
+                .filter(function(d){return d; }); // nodes linked to it
+
+            node.filter(function(d){return nodesToHighlight.indexOf(d) >= 0; })
+            // gives you the nodes which have new arrays mapped to it
+                .style('fill', '#d66742');
+            link.style('stroke', function(link_d){
+                return link_d.source === d | link_d.target === d ? '#d69265' : null;
+            });
+        })
+        .on('mouseout', function(d){
+            node.style('fill', null);
+            link.style('stroke', null);
+        });
+
+    var link = svg.append('g')
+        .attr('class', 'links')
+        .selectAll('path')
+        .data(graph.links)
+        .enter()
+        .attr('fill', null)
+        .attr('stroke','rgba(100,107,110,0.8)')
+        .attr('stroke-opacity', '0.4')
+        .attr('d', function(d) {
+            return ['M', d.source.x, height, 'A', (d.source.x - d.target.x) / 2, ',',
+                (d.source.x - d.target.x) / 2, 0, 0, ',',
+                d.source.x < d.target.x ? 1 : 0, d.target.x, ',', height].join(' ');
+        });
+        .attr('stroke-width', function(d){ return linkwidth(d.count); })
+        .on('mouseover', function(d){
+            link.style('stroke', null);
+            d3.select(this).style('stroke', '#d69265');
+            node.style('fill', function(node_d){
+                return node_d === d.source || node_d === d.target ? '#d66742' : null;
+            });
+        })
+        .on('mouseout', function(d){
+            link.style('stroke', null);
+            node.style('fill', null);
+        });
+
+        });
     // graph.sort(function(a,b){
     //     return a.APPEARANCES - b.APPEARANCES
-    });
+    // });
 
 
     // // compute links
